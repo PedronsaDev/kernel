375f16e636b81a4c21c972ac5894c30097a4c2fb
PedronsaDev <pedro.achilles@live.com>
2026-04-04 09:39:52 -0300

    New branch

--- kernel/main.c
+++ kernel/main.c
@@ -3,7 +3,6 @@
 // O linker manda o contador de programa para o binário daqui.
 void kmain(void) {
     serial_init();
-    serial_puts("Ambiente preparado\n");
     serial_puts("Bem-vindo ao UFSKernel!\n");
     serial_puts("Executando em modo ARM bare-metal no QEMU.\n");
     
