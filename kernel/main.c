eb5d8aeddcdff449099b49aa157c7815f2aa8f01
Pedro Augusto <109170339+pagfrare@users.noreply.github.com>
2026-04-04 16:29:59 -0300

    Add files via upload

--- kernel/main.c
+++ kernel/main.c
@@ -3,7 +3,8 @@
 // O linker manda o contador de programa para o binário daqui.
 void kmain(void) {
     serial_init();
-    serial_puts("Bem-vindo ao UFSKernel!\n");
+    serial_puts("Bem-vindo ao grupo de memoria!\n");
+	serial_puts("E aqui onde comeca o choro\n");
     serial_puts("Executando em modo ARM bare-metal no QEMU.\n");
 
     return;
