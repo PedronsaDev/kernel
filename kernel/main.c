08cc084798bc01dd5574070a5485ddb1ee9996c9
PedronsaDev <pedro.achilles@live.com>
2026-04-22 16:21:24 -0300

    Implementacao uart buffer

--- kernel/main.c
+++ kernel/main.c
@@ -6,17 +6,5 @@ void kmain(void) {
     serial_init();
     serial_puts("Bem-vindo ao UFSKernel!\n");
     serial_puts("Executando em modo ARM bare-metal no QEMU.\n");
-    
     uart_init();
-    uart_puts("Kernel Booted. Type something and I will echo it back after a delay...\n");
-
-    while (1) {
-        for (volatile int i = 0; i < 10000000; i++); 
-
-        char c = uart_getc(); 
-        
-        uart_puts("\nI received: ");
-        uart_putc(c);
-        uart_puts("\n");
-    }
 }
