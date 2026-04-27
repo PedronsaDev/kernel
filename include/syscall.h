647133e49ca8d60710e3ebb177472dbd302a14a6
Vincentius004 <56077081+Vincentius004@users.noreply.github.com>
2026-04-27 16:43:46 -0300

    Criação da syscall table + adequações para funcionamento

--- include/syscall.h
+++ include/syscall.h
@@ -1,10 +1,9 @@
-#define FORK_CALL 1
-#define EXEC_CALL 2
-#define WAIT_CALL 3
-#define EXIT_CALL 4
-#define MALLOC_CALL 5
-#define PRINTF_CALL 6
+#define PRINTF_CALL 0
+#define MALLOC_CALL 1
+#define FORK_CALL 2
+#define EXEC_CALL 3
+#define WAIT_CALL 4
+#define EXIT_CALL 5
 
 
-int syscall(int id);
 int printf(const char *s, int sizeBuffer);
