ba1c048e0b94d8d828c91bcb1e5a37b56e2ea871
PedronsaDev <pedro.achilles@live.com>
2026-04-28 09:19:10 -0300

    Atualizacao do main.c

--- kernel/main.c
+++ kernel/main.c
@@ -10,16 +10,20 @@ void kmain(void) {
 
     uart_init();
     gic_init();
+
+    // Habilita as interrupcoes do ID 33(UART)
+    // e coloca o target na CPU 0
     gic_enable_interrupt(33);
 
-    uart_puts("GIC e UART rodando em modo manual.\n");
+    uart_puts("UART e GIC rodando em modo manual.\n");
     uart_puts("Digite alguma coisa. O GIC deve detectar...\n");
 
+    // Precisa linkar com a vector table
     while (1) {
         if (gic_is_pending(33)) {
-            uart_puts("GIC detectou IRQ 33 pendente.\n");
+            // uart_puts("GIC detectou IRQ 33 pendente.\n");
 
-            // Simulando o escalonador
+            // Simulando a vector table
             uint32_t iar = gic_acknowledge_interrupt();
             if ((iar & 0x3FF) == 33) {
                 uart_handler();
@@ -29,10 +33,11 @@ void kmain(void) {
 
             // Verifica se conseguimos escrever no buffer
             char c = uart_getc();
-            uart_puts("Character recuperado do buffer com sucesso: ");
+            uart_puts("Caracter lido do buffer: ");
             uart_putc(c);
             uart_puts("\n");
         }
+
         // Adiciona um pouco de delay
         for (volatile int i = 0; i < 500000; i++);
     }
