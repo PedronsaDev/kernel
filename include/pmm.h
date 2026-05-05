e3972934a1667b0aad4f0842b5065c3da0fb5a3e
Pedro Augusto <pagfrare@gmail.com>
2026-05-05 11:37:18 -0300

    Mudança no pmm e unificação das headers

--- include/pmm.h
+++ include/pmm.h
@@ -5,8 +5,21 @@
 
 #include <stdint.h>
 
-// Inicializa o gerenciador com a base da RAM e seu tamanho total
-void pmm_init(uint32_t start_addr, uint32_t size);
+#define RAM_START        0x40000000  // Início da RAM no QEMU
+
+/* Tamanho da RAM - Vamos definir 128MB por padrão */
+#define RAM_SIZE         (128 * 1024 * 1024) 
+
+#define RAM_END          (RAM_START + RAM_SIZE)
+
+/* Endereço da UART (que você já usa no serial.c) */
+#define UART0_ADDRESS    0x09000000
+
+/* Tamanho de cada página/bloco de memória (4 KB) */
+#define PAGE_SIZE        4096 
+
+// Inicializa o gerenciador a partir do end
+void pmm_init();
 
 // Aloca um bloco de 4KB e retorna seu endereço físico
 void* pmm_alloc_block();
--- kernel/pmm.c
+++ kernel/pmm.c
@@ -1,42 +1,43 @@
-// Gerencia o array de bits (Bitmap).
-#include "mem_layout.h"
+// Gerencia o array de bits (Bitmap)
 #include "pmm.h"
 
-extern uint8_t __end[]; // Símbolo vindo do linker script, representa o final do kernel
+ extern uint8_t __end[]; // Símbolo vindo do linker script, representa o final do kernel
 
 uint32_t *bitmap;
 
 void pmm_init() {
-    bitmap = (uint32_t *) __end;
+    bitmap = (uint32_t *) __end; //tem que alinhar aqui
 
-    uint32_t num_frames  = RAM_SIZE / PAGE_SIZE;
-    uint32_t bitmap_size = num_frames / 8;
-
-    for (uint32_t i = 0; i < (bitmap_size / 4); i++) {
+    uint32_t num_pages  = RAM_SIZE / PAGE_SIZE;
+    uint32_t bitmap_size = num_pages / 32;
+ 
+    for (uint32_t i = 0; i < bitmap_size; i++) {
         bitmap[i] = 0xFFFFFFFF;
     }
 
-    uintptr_t free_mem_start = (uintptr_t) __end + bitmap_size;
+    uintptr_t free_mem_start = (uintptr_t) __end + bitmap_size * sizeof(uint32_t);
 
     for(uintptr_t addr = free_mem_start; addr < RAM_END; addr += PAGE_SIZE) {
-        pmm_free_frame(addr);
+        pmm_free_page(addr);
     }
 
 }
 
 
-void pmm_free_frame(uintptr_t addr) {
-    uint32_t frame = addr / PAGE_SIZE;
-    uint32_t idx = frame / 32;
-    uint32_t bit = frame % 32;
+void pmm_free_page(uintptr_t addr) {
+    uint32_t page = addr / PAGE_SIZE;
+    uint32_t idx = page / 32;
+    uint32_t bit = page % 32;
 
-    bitmap[idx] &= ~(1 << bit);
+    uint32_t mask = ~(1 << bit);
+    bitmap[idx] &= mask;
 }
 
-void pmm_alloc_frame_at(uintptr_t addr) {
-    uint32_t frame = addr / PAGE_SIZE;
-    uint32_t idx = frame / 32;
-    uint32_t bit = frame % 32;
-
-    bitmap[idx] |= (1 << bit); 
+void pmm_alloc_page_at(uintptr_t addr) {
+    uint32_t page = addr / PAGE_SIZE;
+    uint32_t idx = page / 32;
+    uint32_t bit = page % 32;
+    
+    uint32_t mask = (1 << bit);
+    bitmap[idx] |= mask; 
 }
